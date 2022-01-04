758ea9f19c72906447bf240ce6e81056f95e4937
soloexploder <soloexploder@gmail.com>
2022-01-04 17:23:03 +0300

    Initial Commit

---
+++ LoginingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginingApp
+//
+//  Created by Даниил Хантуров on 04.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
